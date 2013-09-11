9acc0e808957a3ac9acadcee5aa80554382a3884
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-09-11 17:07:38 -0500

    Initial commit.

---
+++ remove_dup_devices.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+duplicatesFile = ARGV[1]
+duplicatesData = File.open(duplicatesFile, "r").read
+
+duplicateDevices = []
+duplicatesData.each_line do |line|
+	duplicateDevices << /'(.+)'/.match(line)[1]
+end
+
+devicesFile = ARGV[0]
+devicesData = File.open(devicesFile, "r").read
+
+newDeviceList = []
+devicesData.each_line do |line|
+	device = line.split[0]
+	if ! duplicateDevices.include?(device) 
+		newDeviceList << line
+	end
+end
+
+File.open(ARGV[2], 'w') do |file|
+	file.puts newDeviceList
+end
